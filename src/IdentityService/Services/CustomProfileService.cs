da6ce8bf1e42b6375d8625aa208d59c6eb7662e2
Sam Gharcheh <sam.gharacheh@outlook.com>
2024-02-06 22:46:43 +0800

    Added Identity services

---
+++ src/IdentityService/Services/CustomProfileService.cs
@@ -0,0 +1,47 @@
+﻿using System.Security.Claims;
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Services;
+using IdentityModel;
+using IdentityService.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityService.Services;
+
+public class CustomProfileService : IProfileService
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public CustomProfileService(UserManager<ApplicationUser> _userManager)
+    {
+        this._userManager = _userManager;
+    }
+
+    public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+    {
+        var user = await _userManager.GetUserAsync(context.Subject);
+        if (user != null)
+        {
+            var existingClams = await _userManager.GetClaimsAsync(user);
+
+
+            if (user.UserName != null)
+            {
+                // Adding new custom claim
+                var claims = new List<Claim>()
+                {
+                    new Claim("username", user.UserName),
+                };
+
+                context.IssuedClaims.AddRange(claims);
+
+                // Adding Full name of User in the existing Claim
+                context.IssuedClaims.Add(existingClams.FirstOrDefault(x => x.Type == JwtClaimTypes.Name));
+            }
+        }
+    }
+
+    public Task IsActiveAsync(IsActiveContext context)
+    {
+        return Task.CompletedTask;
+    }
+}
